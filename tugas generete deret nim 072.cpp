9574c50e97933cc1afa599b048274499f54911e8
dwiariprayogo MIB 19 <61019329+dwiariprayogo@users.noreply.github.com>
2020-10-21 21:51:16 +0700

    Add files via upload

---
+++ tugas generete deret nim 072.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int deret, n, a, z;
+
+    cout << "jumlah deret yang mucul :";
+    cin>>deret; z=7;
+    for(a=1; a<deret; a++)
+    {
+    cout <<z<<"";z=z+7;
+
+    };
+    return 0;
+}
